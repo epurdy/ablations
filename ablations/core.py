c3193d241c802e9f1d1a4ffeca76a8b162a91c55
Eric Purdy <eric.purdy@gmail.com>
2022-10-16 03:44:28 +0000

    many modifications, now allows ablating only a single position

--- ablations/core.py
+++ ablations/core.py
@@ -1,11 +1,22 @@
 import abc
+from collections import defaultdict
 from functools import partial
 
+
 import datasets
+import einops
 import torch
 import tqdm
 from easy_transformer import EasyTransformer
 
+
+def return_zero_on_index_error(fn):
+    def wrapped_fn(*args, **kwargs):
+        try:
+            retval = fn(*args, **kwargs)
+        except:
+            return 0
+    return wrapped_fn
 
 class TaskHarness(metaclass=abc.ABCMeta):
     """This is an abstract class that should be subclassed to define how
@@ -21,19 +32,20 @@ class TaskHarness(metaclass=abc.ABCMeta):
 
 
 class ModelAblator:
-    def __init__(self, model_name='gpt2', tasks=None):
-        device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    def __init__(self, model_name='gpt2'):
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.model = EasyTransformer.from_pretrained(model_name)
-        self.model = model.to(device)
+        self.model = self.model.to(self.device)
         torch.set_grad_enabled(False)
 
-        if tasks is None:
-            tasks = []
-        self.tasks = tasks
+        self.tasks = []
 
         self.example_cache = None
         self.average_activations = None
-        
+
+    def register_task(self, task):
+        self.tasks.append(task)
+
     def cache_activations(self):
         self.model.cfg.use_attn_result = True
         self.example_cache = {}
@@ -51,12 +63,16 @@ class ModelAblator:
                     print(
                         act_name,
                         self.example_cache[task_name][key][act_name].shape)
-                
+
         self.model.cfg.use_attn_result = False
 
     def calculate_mean_activations(
             self,
             dataset_name='stas/openwebtext-10k'):
+
+        print('Calculating mean activations...')
+        self.model.cfg.use_attn_result = True
+
         dataset = datasets.load_dataset(dataset_name, split='train')
         dataloader = torch.utils.data.DataLoader(dataset, batch_size=1)
 
@@ -74,7 +90,7 @@ class ModelAblator:
         self.model.reset_hooks()
         for name, hook in self.model.hook_dict.items():
             hook_suffix = name.split('.')[-1]
-            if hook_suffix in ['hook_v',
+            if hook_suffix in ['hook_result',
                                'hook_attn_out',
                                'hook_mlp_out']:
                 # This adds a hook to count a running total
@@ -86,7 +102,7 @@ class ModelAblator:
             tokens = self.model.tokenizer.encode(
                 batch['text'][0],
                 truncation=True,
-                return_tensors='pt').to(device)
+                return_tensors='pt').to(self.device)
             logits = self.model(tokens)
             count_samples += len(tokens)
             if count_samples > max_samples:
@@ -95,29 +111,46 @@ class ModelAblator:
         average_acts = {}
         for name, hook in self.model.hook_dict.items():
             hook_suffix = name.split('.')[-1]
-            if hook_suffix in ['hook_v', 'hook_attn_out', 'hook_mlp_out']:
+            if hook_suffix in ['hook_result', 'hook_attn_out', 'hook_mlp_out']:
                 average_acts[hook.name] = hook.ctx['running_total']/hook.ctx['count']
         print('Average act keys:', average_acts.keys())
         self.model.reset_hooks(clear_contexts=True)
 
         self.average_activations = average_acts
-        
+        self.model.cfg.use_attn_result = False
+
     def zero_ablate_hook(self, result, hook):
         result[:] = 0
         return result
 
-    def zero_ablate_head_hook(self, result, hook, head_idx):
+    def zero_ablate_head_hook(self, result, hook, *, head_idx):
         result[:, :, head_idx] = 0
         return result
+
+    def zero_ablate_position_hook(self, result, hook, *, posn_idx):
+        result[:, posn_idx] = 0
+
+    def zero_ablate_head_position_hook(self, result, hook, *, posn_idx, head_idx):
+        result[:, posn_idx, head_idx] = 0
 
     def mean_ablate_hook(self, result, hook):
         result[:] = self.average_activations[hook.name]
         return result
 
-    def mean_ablate_head_hook(self, result, hook, head_idx):
+    def mean_ablate_head_hook(self, result, hook, *, head_idx):
         result[:, :, head_idx] = self.average_activations[hook.name][head_idx]
         return result
-        
+
+    def mean_ablate_position_hook(self, result, hook, *, posn_idx):
+        result[:, posn_idx] = self.average_activations[hook.name]
+        return result
+
+    def mean_ablate_head_position_hook(self, result, hook, *,
+                                       posn_idx, head_idx):
+        result[:, posn_idx, head_idx] = self.average_activations[hook.name]
+        return result
+
+
     def ablate_component(self, component_name, hook):
         logit_diffs = {}
         for task in self.tasks:
@@ -132,35 +165,65 @@ class ModelAblator:
                 )
                 self.model.reset_hooks()
                 clean_logit_diff = task.get_logit_diff(
-                    logits=logits, key=key)
+                    logits=clean_logits, key=key)
                 ablated_logit_diff = task.get_logit_diff(
-                    logits=logits, key=key)
+                    logits=ablated_logits, key=key)
                 logit_diffs[task_name][key] = (
                     ablated_logit_diff - clean_logit_diff
                 ).item()
 
-    def zero_ablate_attention_layer(self, layer_idx):
+        return logit_diffs
+
+    def zero_ablate_attention_layer(self, *, layer_idx):
         logit_diffs = self.ablate_component(
             component_name=f'blocks.{layer_idx}.hook_attn_out',
             hook=self.zero_ablate_hook,
         )
         return logit_diffs
 
-    def zero_ablate_attention_head(self, layer_idx, head_idx):
+    @return_zero_on_index_error
+    def zero_ablate_attention_layer_at_posn(self, *, layer_idx, posn_idx):
         logit_diffs = self.ablate_component(
-            component_name=f'blocks.{layer_idx}.hook_v',
+            component_name=f'blocks.{layer_idx}.hook_attn_out',
+            hook=partial(self.zero_ablate_position_hook,
+                         posn_idx=posn_idx),
+        )
+        return logit_diffs
+
+    def zero_ablate_attention_head(self, *, layer_idx, head_idx):
+        logit_diffs = self.ablate_component(
+            component_name=f'blocks.{layer_idx}.attn.hook_result',
             hook=partial(self.zero_ablate_head_hook, head_idx=head_idx),
         )
         return logit_diffs
+
+    @return_zero_on_index_error
+    def zero_ablate_attention_head_at_posn(self, *, layer_idx, head_idx, posn_idx):
+        logit_diffs = self.ablate_component(
+            component_name=f'blocks.{layer_idx}.attn.hook_result',
+            hook=partial(self.zero_ablate_head_position_hook,
+                         head_idx=head_idx,
+                         posn_idx=posn_idx),
+        )
+        return logit_diffs
     
-    def zero_ablate_mlp_layer(self, layer_idx):
+    def zero_ablate_mlp_layer(self, *, layer_idx):
         logit_diffs = self.ablate_component(
             component_name=f'blocks.{layer_idx}.hook_mlp_out',
             hook=self.zero_ablate_hook,
         )
         return logit_diffs
 
-    def mean_ablate_attention_layer(self, layer_idx):
+    @return_zero_on_index_error
+    def zero_ablate_mlp_layer_at_posn(self, *, layer_idx, posn_idx):
+        logit_diffs = self.ablate_component(
+            component_name=f'blocks.{layer_idx}.hook_mlp_out',
+            hook=partial(self.zero_ablate_position_hook,
+                         posn_idx=posn_idx),
+        )
+        return logit_diffs
+    
+    def mean_ablate_attention_layer(self, *, layer_idx):
         assert self.average_activations is not None, 'must calculate average activations before calling this function'
         logit_diffs = self.ablate_component(
             component_name=f'blocks.{layer_idx}.hook_attn_out',
@@ -168,15 +231,40 @@ class ModelAblator:
         )
         return logit_diffs
 
-    def mean_ablate_attention_head(self, layer_idx, head_idx):
+    @return_zero_on_index_error
+    def mean_ablate_attention_layer_at_posn(self, *,
+                                            layer_idx, posn_idx):
         assert self.average_activations is not None, 'must calculate average activations before calling this function'
         logit_diffs = self.ablate_component(
-            component_name=f'blocks.{layer_idx}.hook_v',
-            hook=partial(self.mean_ablate_head_hook, head_idx=head_idx),
+            component_name=f'blocks.{layer_idx}.hook_attn_out',
+            hook=partial(self.mean_ablate_position_hook,
+                         posn_idx=posn_idx),
         )
         return logit_diffs
     
-    def mean_ablate_mlp_layer(self, layer_idx):
+    def mean_ablate_attention_head(self, *, layer_idx, head_idx):
+        assert self.average_activations is not None, 'must calculate average activations before calling this function'
+        logit_diffs = self.ablate_component(
+            component_name=f'blocks.{layer_idx}.attn.hook_result',
+            hook=partial(self.mean_ablate_head_hook, head_idx=head_idx),
+        )
+        return logit_diffs
+
+    @return_zero_on_index_error
+    def mean_ablate_attention_head_at_posn(self, *,
+                                           layer_idx,
+                                           head_idx,
+                                           posn_idx):
+        assert self.average_activations is not None, 'must calculate average activations before calling this function'
+        logit_diffs = self.ablate_component(
+            component_name=f'blocks.{layer_idx}.attn.hook_result',
+            hook=partial(self.mean_ablate_head_position_hook,
+                         head_idx=head_idx,
+                         posn_idx=posn_idx),
+        )
+        return logit_diffs
+    
+    def mean_ablate_mlp_layer(self, *, layer_idx):
         assert self.average_activations is not None, 'must calculate average activations before calling this function'
         logit_diffs = self.ablate_component(
             component_name=f'blocks.{layer_idx}.hook_mlp_out',
@@ -184,4 +272,202 @@ class ModelAblator:
         )
         return logit_diffs
 
+    @return_zero_on_index_error
+    def mean_ablate_mlp_layer_at_posn(self, *, layer_idx, posn_idx):
+        assert self.average_activations is not None, 'must calculate average activations before calling this function'
+        logit_diffs = self.ablate_component(
+            component_name=f'blocks.{layer_idx}.hook_mlp_out',
+            hook=partial(self.mean_ablate_position_hook,
+                         posn_idx=posn_idx),
+        )
+        return logit_diffs
     
+    def run_mean_ablation_sweep(self):
+        assert self.average_activations is not None, 'must calculate average activations before calling this function'
+
+        print('Ablating individual attention heads')
+        all_head_logit_diffs = {}
+        for layer_idx in tqdm.tqdm(range(self.model.cfg.n_layers)):
+            for head_idx in range(self.model.cfg.n_heads):
+                logit_diffs = self.mean_ablate_attention_head(
+                    layer_idx=layer_idx, head_idx=head_idx
+                )
+                all_head_logit_diffs[layer_idx, head_idx] = logit_diffs
+
+        print('Ablating attention layers')
+        all_attn_logit_diffs = {}
+        for layer_idx in tqdm.tqdm(range(self.model.cfg.n_layers)):
+            logit_diffs = self.mean_ablate_attention_layer(
+                layer_idx=layer_idx,
+            )
+            all_attn_logit_diffs[layer_idx] = logit_diffs
+
+
+        print('Ablating MLP layers')
+        all_mlp_logit_diffs = {}
+        for layer_idx in tqdm.tqdm(range(self.model.cfg.n_layers)):
+            logit_diffs = self.mean_ablate_mlp_layer(
+                layer_idx=layer_idx,
+            )
+            all_mlp_logit_diffs[layer_idx] = logit_diffs
+
+        return dict(
+            heads=all_head_logit_diffs,
+            mlps=all_mlp_logit_diffs,
+            attns=all_attn_logit_diffs
+        )
+
+    def run_positional_mean_ablation_sweep(self):
+        assert self.average_activations is not None, 'must calculate average activations before calling this function'
+
+        max_posn = 0
+        for task in self.tasks:
+            for example in task.get_examples().values():
+                max_posn = max(
+                    max_posn,
+                    len(self.model.tokenizer.encode(example)))
+        print('max_posn', max_posn)
+        
+        print('Ablating individual attention heads')
+        all_head_logit_diffs = {}
+        for layer_idx in tqdm.tqdm(range(self.model.cfg.n_layers)):
+            for head_idx in range(self.model.cfg.n_heads):
+                for posn_idx in range(max_posn):
+                    logit_diffs = self.mean_ablate_attention_head_at_posn(
+                        layer_idx=layer_idx, head_idx=head_idx,
+                        posn_idx=posn_idx,
+                    )
+                    all_head_logit_diffs[posn_idx, layer_idx, head_idx] = (
+                        logit_diffs
+                    )
+
+        print('Ablating attention layers')
+        all_attn_logit_diffs = {}
+        for layer_idx in tqdm.tqdm(range(self.model.cfg.n_layers)):
+            for posn_idx in range(max_posn):
+                logit_diffs = self.mean_ablate_attention_layer_at_posn(
+                    layer_idx=layer_idx,
+                    posn_idx=posn_idx,
+                )
+                all_attn_logit_diffs[posn_idx, layer_idx] = logit_diffs
+
+
+        print('Ablating MLP layers')
+        all_mlp_logit_diffs = {}
+        for layer_idx in tqdm.tqdm(range(self.model.cfg.n_layers)):
+            for posn_idx in range(max_posn):
+                logit_diffs = self.mean_ablate_mlp_layer_at_posn(
+                    layer_idx=layer_idx,
+                    posn_idx=posn_idx,
+                )
+                all_mlp_logit_diffs[posn_idx, layer_idx] = logit_diffs
+
+        return dict(
+            heads=all_head_logit_diffs,
+            mlps=all_mlp_logit_diffs,
+            attns=all_attn_logit_diffs
+        )
+    
+
+    def run_zero_ablation_sweep(self):
+        assert self.average_activations is not None, 'must calculate average activations before calling this function'
+
+        print('Ablating individual attention heads')
+        all_head_logit_diffs = {}
+        for layer_idx in tqdm.tqdm(range(self.model.cfg.n_layers)):
+            for head_idx in range(self.model.cfg.n_heads):
+                logit_diffs = self.zero_ablate_attention_head(
+                    layer_idx=layer_idx, head_idx=head_idx
+                )
+                all_head_logit_diffs[layer_idx, head_idx] = logit_diffs
+
+        print('Ablating attention layers')
+        all_attn_logit_diffs = {}
+        for layer_idx in tqdm.tqdm(range(self.model.cfg.n_layers)):
+            logit_diffs = self.zero_ablate_attention_layer(
+                layer_idx=layer_idx,
+            )
+            all_attn_logit_diffs[layer_idx] = logit_diffs
+
+
+        print('Ablating MLP layers')
+        all_mlp_logit_diffs = {}
+        for layer_idx in tqdm.tqdm(range(self.model.cfg.n_layers)):
+            logit_diffs = self.zero_ablate_mlp_layer(
+                layer_idx=layer_idx,
+            )
+            all_mlp_logit_diffs[layer_idx] = logit_diffs
+
+        return dict(
+            heads=all_head_logit_diffs,
+            mlps=all_mlp_logit_diffs,
+            attns=all_attn_logit_diffs
+        )
+
+    def run_positional_zero_ablation_sweep(self):
+        max_posn = 0
+        for task in self.tasks:
+            for example in task.get_examples().values():
+                max_posn = max(
+                    max_posn,
+                    len(self.model.tokenizer.encode(example)))
+        print('max_posn', max_posn)
+        
+        print('Ablating individual attention heads')
+        all_head_logit_diffs = {}
+        for layer_idx in tqdm.tqdm(range(self.model.cfg.n_layers)):
+            for head_idx in range(self.model.cfg.n_heads):
+                for posn_idx in range(max_posn):
+                    logit_diffs = self.zero_ablate_attention_head_at_posn(
+                        layer_idx=layer_idx, head_idx=head_idx,
+                        posn_idx=posn_idx,
+                    )
+                    all_head_logit_diffs[posn_idx, layer_idx, head_idx] = (
+                        logit_diffs
+                    )
+
+        print('Ablating attention layers')
+        all_attn_logit_diffs = {}
+        for layer_idx in tqdm.tqdm(range(self.model.cfg.n_layers)):
+            for posn_idx in range(max_posn):
+                logit_diffs = self.zero_ablate_attention_layer_at_posn(
+                    layer_idx=layer_idx,
+                    posn_idx=posn_idx,
+                )
+                all_attn_logit_diffs[posn_idx, layer_idx] = logit_diffs
+
+        print('Ablating MLP layers')
+        all_mlp_logit_diffs = {}
+        for layer_idx in tqdm.tqdm(range(self.model.cfg.n_layers)):
+            for posn_idx in range(max_posn):
+                logit_diffs = self.zero_ablate_mlp_layer_at_posn(
+                    layer_idx=layer_idx,
+                    posn_idx=posn_idx,
+                )
+                all_mlp_logit_diffs[posn_idx, layer_idx] = logit_diffs
+
+        return dict(
+            heads=all_head_logit_diffs,
+            mlps=all_mlp_logit_diffs,
+            attns=all_attn_logit_diffs
+        )
+    
+    def summarize_logit_diffs(self, logit_diffs):
+        scored_components = defaultdict(list)
+        for component_type in logit_diffs:
+            for index in logit_diffs[component_type]:
+                for task_name in logit_diffs[component_type][index]:
+                    for example_key in logit_diffs[component_type][index][task_name]:
+                        diff = logit_diffs[component_type][index][task_name][example_key]
+                        scored_components[task_name, example_key].append(
+                            (diff, component_type, index)
+                        )
+
+        for task_name, example_key in scored_components:
+            scored_components[task_name, example_key].sort()
+            print('-' * 80)
+            print('Task:', task_name)
+            print('Example key:', example_key)
+            for i in range(10):
+                diff, typ, idx = scored_components[task_name, example_key][i]
+                print(f'Component: {typ} {idx} {diff:.3f}')
--- ablations/example.py
+++ ablations/example.py
@@ -2,11 +2,11 @@ from ablations.core import ModelAblator, TaskHarness
 
 
 class FirstPronounGenderTask(TaskHarness):
-    def __init__(self, model):
-        he_tokens = model.tokenizer.encode(" he")
+    def __init__(self, ablator):
+        he_tokens = ablator.model.tokenizer.encode(" he")
         assert len(he_tokens) == 1
         self.he_index = he_tokens[0]
-        she_tokens = model.tokenizer.encode(" she")
+        she_tokens = ablator.model.tokenizer.encode(" she")
         assert len(she_tokens) == 1
         self.she_index = she_tokens[0]
 
@@ -36,11 +36,11 @@ class FirstPronounGenderTask(TaskHarness):
 
 
 class SecondPronounGenderTask(TaskHarness):
-    def __init__(self, model):
-        him_tokens = model.tokenizer.encode(" him")
+    def __init__(self, ablator):
+        him_tokens = ablator.model.tokenizer.encode(" him")
         assert len(him_tokens) == 1
         self.him_index = him_tokens[0]
-        her_tokens = model.tokenizer.encode(" her")
+        her_tokens = ablator.model.tokenizer.encode(" her")
         assert len(her_tokens) == 1
         self.her_index = her_tokens[0]
 
@@ -70,12 +70,22 @@ class SecondPronounGenderTask(TaskHarness):
 
 
 def main():
-    task1 = FirstPronounGenderTask(model)
-    task2 = SecondPronounGenderTask(model)
+    ablator = ModelAblator()
+    ablator.register_task(FirstPronounGenderTask(ablator))
+    ablator.register_task(SecondPronounGenderTask(ablator))
+    ablator.calculate_mean_activations()
 
-    ablator = ModelAblator(tasks=[task1, task2])
+    mean_logit_diffs = ablator.run_mean_ablation_sweep()
+    zero_logit_diffs = ablator.run_zero_ablation_sweep()
+    positional_mean_logit_diffs = ablator.run_positional_mean_ablation_sweep()
+    positional_zero_logit_diffs = ablator.run_positional_zero_ablation_sweep()
+    ablator.summarize_logit_diffs(mean_logit_diffs)
+    ablator.summarize_logit_diffs(zero_logit_diffs)
+    ablator.summarize_logit_diffs(positional_mean_logit_diffs)
+    ablator.summarize_logit_diffs(positional_zero_logit_diffs)
 
-
+    import ipdb
+    ipdb.set_trace()
 
 if __name__ == '__main__':
     main()
